56123df35c0f774fecbd5b0e3228adf2a40a26f4
HU KE <403392669@qq.com>
2024-07-28 13:18:32 +0800

    feat: web3 storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import getWeb3 from './utils/getWeb3';
-import DeFiContract from './contracts/DeFiContract.json';
+import React, { useEffect } from 'react';
+import { create } from '@web3-storage/w3up-client'
 
 const App = () => {
-  const [web3, setWeb3] = useState(null);
-  const [accounts, setAccounts] = useState(null);
-  const [contract, setContract] = useState(null);
-  const [balance, setBalance] = useState('0');
-  const [depositAmount, setDepositAmount] = useState('');
+
+  const handleFileChange = async(e) => {
+    // @ts-ignore
+    let file = e?.target?.files[0]
+    console.log('file', file)
+    const client = await create()
+    await client.login('hu-ke@hotmail.com')
+    await client.setCurrentSpace('did:key:z6MkgavdgRcmrjkXDt6HbKc5926cn8r3cqGFkG2JiWst4DBd')
+    const directoryCid = await client.uploadFile(file)
+    console.log('directoryCid', directoryCid)
+  }
 
   useEffect(() => {
-    const init = async () => {
-      try {
-        const web3 = await getWeb3();
-        const accounts = await web3.eth.getAccounts();
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = DeFiContract.networks[networkId];
-        const instance = new web3.eth.Contract(
-          DeFiContract.abi,
-          deployedNetwork && deployedNetwork.address,
-        );
-
-        setWeb3(web3);
-        setAccounts(accounts);
-        setContract(instance);
-
-        const balance = await instance.methods.getBalance().call({ from: accounts[0] });
-        setBalance(web3.utils.fromWei(balance, 'ether'));
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    init();
+    const init = async() => {
+      // console.log('init')
+      // const client = await create()
+      // // await client.authorize('hu-ke@hotmail.com')
+      // console.log(client)
+      // try {
+      //   await client.login('hu-ke@hotmail.com')
+      //   // await client.registerSpace('hu-ke@hotmail.com', { provider: 'did:key:z6MkgavdgRcmrjkXDt6HbKc5926cn8r3cqGFkG2JiWst4DBd' })
+      //   await client.setCurrentSpace('did:key:z6MkgavdgRcmrjkXDt6HbKc5926cn8r3cqGFkG2JiWst4DBd')
+      //   const file = new File(['testcss'], './index.css')
+        
+      //   const directoryCid = await client.uploadFile(file)
+      //   console.log('directoryCid', directoryCid)
+      // } catch (err) {
+      //   console.error('registration failed: ', err)
+      // }
+    }
+    init()
   }, []);
-
-  const handleDeposit = async () => {
-    if (contract) {
-      const amount = web3.utils.toWei(depositAmount, 'ether');
-      await contract.methods.deposit().send({ from: accounts[0], value: amount });
-      const balance = await contract.methods.getBalance().call({ from: accounts[0] });
-      setBalance(web3.utils.fromWei(balance, 'ether'));
-      setDepositAmount('');
-    }
-  };
-
-  const handleWithdraw = async () => {
-    if (contract) {
-      const amount = web3.utils.toWei(depositAmount, 'ether');
-      await contract.methods.withdraw(amount).send({ from: accounts[0] });
-      const balance = await contract.methods.getBalance().call({ from: accounts[0] });
-      setBalance(web3.utils.fromWei(balance, 'ether'));
-      setDepositAmount('');
-    }
-  };
-
-  if (!web3) {
-    return <div>Loading Web3, accounts, and contract...</div>;
-  }
 
   return (
     <div className="App">
-      <h1>DeFi App</h1>
-      <p>Account: {accounts && accounts[0]}</p>
-      <p>Balance: {balance} ETH</p>
-      <div>
-        <input
-          type="text"
-          value={depositAmount}
-          onChange={(e) => setDepositAmount(e.target.value)}
-          placeholder="Amount in ETH"
-        />
-        <button onClick={handleDeposit}>Deposit</button>
-        <button onClick={handleWithdraw}>Withdraw</button>
-      </div>
+      <input type="file" id="fileInput" onChange={handleFileChange} />
     </div>
   );
 };
